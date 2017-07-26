647d9a42309494ebec70063b366ecaa839162b18
aos3618 <liuzhanqi914@163.com>
2017-07-26 09:52:06 +0800

    Update MainActivity.java

--- Shadow/app/src/main/java/com/aos/shadow/MainActivity.java
+++ Shadow/app/src/main/java/com/aos/shadow/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import com.aos.shadowlib.drawable.ShadowDrawable;
 import com.aos.shadowlib.utils.DensityUtil;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         shadowDrawable.setColor(ContextCompat.getColor(this, R.color.my_blur))    //shadowcolor
                 .setOffsetY(DensityUtil.dip2px(this, 5))    //阴影下偏移--offset of the shadow
                 .setRadius(DensityUtil.dip2px(this, 8))     //四角半径--concern of the rectangle
-                .setSoftline(DensityUtil.dip2px(this, 8))   //四周阴影半径-- the shadow of each edge of the rectangle
+                .setEdgeShadowWidth(DensityUtil.dip2px(this, 8))   //四周阴影半径-- the shadow of each edge of the rectangle
                 .setFilterColor(0x56ffffff)                 //中间值，越大阴影越接近设置的值-- the slot to said how close to the shadowcolor
                 .setTopMargin(DensityUtil.dip2px(this, 3))  //上间距--top margin
                 .setParentHeight(DensityUtil.dip2px(this, 200))  //设置要依附的View的高度 -- the height of parent view
